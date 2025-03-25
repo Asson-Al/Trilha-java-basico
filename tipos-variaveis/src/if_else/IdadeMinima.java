f68e968de44cab4946b09b857de27e1306d631b5
oordeP <p2186218@gmail.com>
2025-03-25 13:49:41 -0300

    Criação package e inicio de testes com if_else Com os comentarios.

--- tipos-variaveis/src/if_else/IdadeMinima.java
+++ tipos-variaveis/src/if_else/IdadeMinima.java
@@ -2,15 +2,20 @@ package if_else;
 
 public class IdadeMinima {
     public static void main(String[] args) {
+        //retorna um valor inteiro
         int idade = 21;
+        //retorna um valor verdadeiro e falso
         boolean idadeMinima = idade >= 18;
 
+        //if = Se; sé a condição for correta o código vai rodar.
         if (idadeMinima) {
 
             System.out.println("A sua idade é igual a: " + idade);
             System.out.println("Você esta liberado para entrar na feta pois você é maior de idade.");
 
-        } else {
+        }
+        //else = se não; se a condição a cima foi falsa ira rodar somente a segunda parte do código.
+        else {
             System.out.println("A sua idade é igual a: " + idade);
             System.out.println("Você não esta liberado para entrar na feta pois você é menor de idade.");
         }
