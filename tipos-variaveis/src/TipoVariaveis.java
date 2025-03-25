a443a2e140ef440b483f39cf1fbe1f9444205d13
oordeP <p2186218@gmail.com>
2025-03-25 12:42:53 -0300

    criando o projeto para tipos de variaveis

--- tipos-variaveis/src/TipoVariaveis.java
+++ tipos-variaveis/src/TipoVariaveis.java
@@ -1,5 +1,12 @@
 public class TipoVariaveis {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        // int =  inteiro , double = decimal.
+
+        int idade = 21;
+        double dinheiro = 25.5;
+
+        System.out.println(idade);
+        System.out.println(dinheiro);
     }
 }
