6faebb74f5d3b35d9e8243b80ee3a1ae1e561e81
oordeP <p2186218@gmail.com>
2025-04-01 21:37:02 -0300

    Atualização dos estudos.

--- Sintaxe-Java/src/SobreMim.java
+++ Sintaxe-Java/src/SobreMim.java
@@ -1,24 +1,31 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.Locale;
 
 public class SobreMim {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try {
 
-        System.out.println("Qual a sua altura?");
-        double altura = scanner.nextDouble();
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Digite o seu nome ?");
-        String nome = scanner.next();
+            System.out.println("Qual a sua altura?");
+            double altura = scanner.nextDouble();
 
-        System.out.println("Digite o seu nome ?");
-        String sobrenome = scanner.next();
+            System.out.println("Digite o seu nome ?");
+            String nome = scanner.next();
 
-        System.out.println("Qual a sua idade ?");
-        int idade = scanner.nextInt();
+            System.out.println("Digite o seu nome ?");
+            String sobrenome = scanner.next();
 
-        System.out.println("Olá, me chamo " + nome + " " + sobrenome + ".");
-        System.out.println("Tenho " + idade + " anos.");
-        System.out.println("Minha altura é " + altura + " cm.");
+            System.out.println("Qual a sua idade ?");
+            int idade = scanner.nextInt();
+
+            System.out.println("Olá, me chamo " + nome + " " + sobrenome + ".");
+            System.out.println("Tenho " + idade + " anos.");
+            System.out.println("Minha altura é " + altura + " cm.");
+        } catch (InputMismatchException e) {
+
+            System.err.println("Os campos idade e altura precisam ser numericos.");
+        }
     }
 }
